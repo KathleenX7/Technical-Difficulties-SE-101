d083ea3cf65d81a4d8d80570f687d09cfb100257
georgia-alpajaro <georgia.alpajaro@gmail.com>
2022-11-03 10:10:48 -0400

    added position detection

--- piCameraObjectDetect.py
+++ piCameraObjectDetect.py
@@ -4,6 +4,15 @@ from picamera import PiCamera
 import cv2
 import numpy as np
 
+#not sure what this is
+def isset(v):
+    try:
+        type (eval(v))
+    except:
+        return 0
+    else:
+        return 1
+
 #changes hsv values to rgb values
 def nothing(x):
     pass
@@ -43,6 +52,40 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     #actually taking out the objects of the colors in the frame
     result = cv2.bitwise_and(image, image, mask=mask)
 
+    #///
+    #start of contour finding, maybe test without this first
+    
+    #find contours in threshold (filtered) image
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__.split('.'))
+    
+    #findContours() has different form for opencv2 and opencv3 (includes boolean or not)
+    if major_ver == "2" or major_ver == "3":
+        _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    
+    #find contour with max area (most of that color, likely the object) and store it
+    max_area = 0
+    for cont in contours:
+        area = cv2.contourArea(cont)
+        if area > max_area:
+            max_area = area
+            best_cont = cont
+    
+    #find centroids of best_cont and draw a circle there
+    if isset('best_cont'):
+        M = cv2.moments(best_cont)
+        cx, cy = int(M['m10'] / M['m00'], int(M['m01'] / M['m00']))
+        cv2.circle(frame(cx, cy), 5, 255, -1)
+        print("Central pos: (%d, %d)" % (cx, cy))
+    else:
+        print("[Warning]Tag lost...")
+    
+    #note that cx is the x position of the ping pong ball,
+    #so drive motors until cx is equal/approximately at the 
+    #center of the camera field
+    #///
+    
     cv2.imshow("frame", image)
     cv2.imshow("mask", mask)
     cv2.imshow("result", result)
