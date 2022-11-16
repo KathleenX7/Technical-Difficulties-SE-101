9cf46ab55321615d0b8c2a47bff3507f714c23b3
georgia-alpajaro <georgia.alpajaro@gmail.com>
2022-11-16 17:08:17 -0500

    got rid of old functions

--- pingpong.py
+++ pingpong.py
@@ -20,47 +20,6 @@ error = 15
 xPosition = 100
 middle = 160
 
-#motor functions
-def toggleSwitch():
-    if button0.text=="Start":
-       motorSwitch.on()
-       button0.text="Stop"
-    elif button0.text == "Stop":
-         motorSwitch.off()
-         button0.text = "Start"
-
-def forwardSpeedIncrease():
-    global motorSpeedForward
-    motor.forward(speed=motorSpeedForward)
-    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedForward))
-    motorSpeedForward += 0.1
-    if motorSpeedForward >= 1:
-        motorSpeedForward = 1
-
-def forwardSpeedReduce():
-    global motorSpeedForward
-    motor.forward(speed=motorSpeedForward)
-    print("Reduce speed of motor forward. Current speed = "+ str(motorSpeedForward))
-    motorSpeedForward -= 0.1
-    if motorSpeedForward <= 0:
-        motorSpeedForward = 0
-
-def backwardSpeedIncrease():
-    global motorSpeedBackward
-    motor.forward(speed=motorSpeedBackward)
-    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedBackward))
-    motorSpeedBackward += 0.1
-    if motorSpeedBackward >= 1:
-        motorSpeedBackward = 1
-
-def backwardSpeedReduce():
-    global motorSpeedBackward
-    motor.backward(speed=motorSpeedBackward)
-    print("Reduce speed of motor backward. Current speed = "+ str(motorSpeedBackward))
-    motorSpeedBackward -= 0.1
-    if motorSpeedBackward <= 0:
-        motorSpeedBackward = 0
-
 def isset(v):
     try:
         type (eval(v))
