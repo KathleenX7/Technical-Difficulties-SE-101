76fa51534b1f7dda225ff2f819ec8712fd0fcbdb
georgia-alpajaro <georgia.alpajaro@gmail.com>
2022-11-16 17:02:23 -0500

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,7 +1,61 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
+from time import sleep
+from guizero import App, Text, PushButton
+from gpiozero import Robot, LED
 import numpy as np
+
+#initialize motors
+motor = Robot(left=(4, 14), right=(17, 18))
+motorSwitch = LED(27)
+
+app = App(title="GUI Development", layout="grid", height=600, width=800)
+message = Text(app, text="Dual Motor Control Interface", grid=[4,0])
+
+motorSpeedForward = 0
+motorSpeedBackward = 0
+
+#motor functions
+def toggleSwitch():
+    if button0.text=="Start":
+       motorSwitch.on()
+       button0.text="Stop"
+    elif button0.text == "Stop":
+         motorSwitch.off()
+         button0.text = "Start"
+
+def forwardSpeedIncrease():
+    global motorSpeedForward
+    motor.forward(speed=motorSpeedForward)
+    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedForward))
+    motorSpeedForward += 0.1
+    if motorSpeedForward >= 1:
+        motorSpeedForward = 1
+
+def forwardSpeedReduce():
+    global motorSpeedForward
+    motor.forward(speed=motorSpeedForward)
+    print("Reduce speed of motor forward. Current speed = "+ str(motorSpeedForward))
+    motorSpeedForward -= 0.1
+    if motorSpeedForward <= 0:
+        motorSpeedForward = 0
+
+def backwardSpeedIncrease():
+    global motorSpeedBackward
+    motor.forward(speed=motorSpeedBackward)
+    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedBackward))
+    motorSpeedBackward += 0.1
+    if motorSpeedBackward >= 1:
+        motorSpeedBackward = 1
+
+def backwardSpeedReduce():
+    global motorSpeedBackward
+    motor.backward(speed=motorSpeedBackward)
+    print("Reduce speed of motor backward. Current speed = "+ str(motorSpeedBackward))
+    motorSpeedBackward -= 0.1
+    if motorSpeedBackward <= 0:
+        motorSpeedBackward = 0
 
 def isset(v):
     try:
